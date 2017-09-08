c58efea1dab97d732862ea27fcc90140c45feb0f
uecm <uecmegor@gmail.com>
2017-09-08 12:13:37 +0300

    Initial Commit

---
+++ Connectivity/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Connectivity
+//
+//  Created by user on 9/8/17.
+//  Copyright © 2017 greg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
